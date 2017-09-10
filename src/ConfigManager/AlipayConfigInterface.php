b006806d82e55292df3a38cdbdf6270831928a1f
hsldymq <hsldymq@qq.com>
2017-09-11 00:40:55 +0800

    Config Interface

--- src/ConfigManager/AlipayConfigInterface.php
+++ src/ConfigManager/AlipayConfigInterface.php
@@ -6,4 +6,12 @@ interface AlipayConfigInterface
     public function getAppID(): string;
 
     public function getPartnerID(): string;
+
+    public function getAlipayPublicKey(?string $algo = null): string;
+
+    public function getAppPrivateKey(?string $algo = null): string;
+
+    public function getAppPublicKey(?string $algo = null): string;
+
+    public function getRootCAPath(?string $algo = null): string;
 }
--- src/ConfigManager/WeixinConfigInterface.php
+++ src/ConfigManager/WeixinConfigInterface.php
@@ -4,4 +4,14 @@ namespace Archman\PaymentLib\ConfigManager;
 interface WeixinConfigInterface
 {
     public function getAppID(): string;
+
+    public function getMerchantID(): string;
+
+    public function getRootCAPath(): string;
+
+    public function getClientKeyPath(): string;
+
+    public function getClientCertPath(): string;
+
+    public function getApiKey(): string;
 }
