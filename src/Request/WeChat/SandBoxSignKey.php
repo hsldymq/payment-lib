82ec56dcd894ce4871408d47ca4b7dbeac16a3d0
hsldymq <hsldymq@qq.com>
2017-10-27 12:25:39 +0800

    增加微信沙盒模式请求

---
+++ src/Request/WeChat/SandBoxSignKey.php
@@ -0,0 +1,53 @@
+<?php
+namespace Archman\PaymentLib\Request\WeChat;
+
+use Archman\PaymentLib\ConfigManager\WeChatConfigInterface;
+use Archman\PaymentLib\Request\DataParser;
+use Archman\PaymentLib\Request\RequestOption;
+use Archman\PaymentLib\Request\RequestOptionInterface;
+use Archman\PaymentLib\Request\WeChat\Traits\NonceStrTrait;
+use Archman\PaymentLib\RequestInterface\WeChat\Traits\ResponseHandlerTrait;
+use GuzzleHttp\Psr7\Request;
+use function GuzzleHttp\Psr7\stream_for;
+use GuzzleHttp\Psr7\Uri;
+use Psr\Http\Message\RequestInterface;
+use Archman\PaymentLib\Request\RequestableInterface;
+use Archman\PaymentLib\SignatureHelper\WeChat\Generator;
+
+class SandBoxSignKey implements RequestableInterface
+{
+    use NonceStrTrait;
+    use ResponseHandlerTrait;
+
+    const URI = 'https://api.mch.weixin.qq.com/sandboxnew/pay/getsignkey';
+
+    private $config;
+
+    public function __construct(WeChatConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function makeParameters(): array
+    {
+        $parameters['mch_id'] = $this->config->getMerchantID();
+        $parameters['nonce_str'] = $this->getNonceStr();
+        $parameters['sign'] = (new Generator($this->config))->makeSign($parameters);
+
+        return $parameters;
+    }
+
+    public function prepareRequest(): RequestInterface
+    {
+        $parameters = $this->makeParameters();
+        $request = new Request('POST', new Uri(self::URI));
+        $body = stream_for(DataParser::arrayToXML($parameters));
+
+        return $request->withBody($body);
+    }
+
+    public function prepareRequestOption(): RequestOptionInterface
+    {
+        return new RequestOption();
+    }
+}
--- src/Request/WeChat/Traits/RequestPreparationTrait.php
+++ src/Request/WeChat/Traits/RequestPreparationTrait.php
@@ -1,7 +1,6 @@
 <?php
 namespace Archman\PaymentLib\RequestInterface\WeChat\Traits;
 
-use Archman\PaymentLib\ConfigManager\WeChatConfigInterface;
 use Archman\PaymentLib\Request\DataParser;
 use Archman\PaymentLib\Request\RequestOption;
 use Archman\PaymentLib\Request\RequestOptionInterface;
@@ -9,13 +8,17 @@ use GuzzleHttp\Psr7\Request;
 use function GuzzleHttp\Psr7\stream_for;
 use GuzzleHttp\Psr7\Uri;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
 
 trait RequestPreparationTrait
 {
+    /** @var bool 不调用setEnvironment时,默认向生产环境发起请求 */
+    private $isProduction = true;
+
     public function prepareRequest(): RequestInterface
     {
         $parameters = $this->makeParameters();
-        $request = new Request('POST', new Uri(self::URI));
+        $request = new Request('POST', $this->getUri());
         $body = stream_for(DataParser::arrayToXML($parameters));
 
         return $request->withBody($body);
@@ -30,5 +33,23 @@ trait RequestPreparationTrait
         }
 
         return $option;
+    }
+
+    public function setEnvironment(bool $isProduction): self
+    {
+        $this->isProduction = $isProduction;
+
+        return $this;
+    }
+
+    private function getUri(): UriInterface
+    {
+        $uri = new Uri(self::URI);
+        if (!$this->isProduction) {
+            $path = '/sandboxnew/'.ltrim($uri->getPath(), '/');
+            $uri = $uri->withPath($path);
+        }
+
+        return $uri;
     }
 }
