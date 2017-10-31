3167a43a5f84f06873b5aedf0eea3122c2acad0a
hsldymq <hsldymq@qq.com>
2017-11-01 00:30:47 +0800

    增加撤销接口类

---
+++ src/Request/Alipay/TradeCancel.php
@@ -0,0 +1,71 @@
+<?php
+namespace Archman\PaymentLib\RequestInterface\Alipay;
+
+use Archman\PaymentLib\ConfigManager\AlipayConfigInterface;
+use Utils\PaymentVendor\RequestInterface\Alipay\Traits\OpenAPIRequestPreparationTrait;
+use Archman\PaymentLib\RequestInterface\Alipay\Traits\OpenAPIResponseHandlerTrait;
+use Utils\PaymentVendor\RequestInterface\Alipay\Traits\ParametersMakerTrait;
+use Archman\PaymentLib\Request\ParameterHelper;
+use Archman\PaymentLib\Request\RequestableInterface;
+
+/**
+ * 统一收单交易撤销接口.
+ * @link https://docs.open.alipay.com/api_1/alipay.trade.cancel/
+ */
+class TradeCancel implements RequestableInterface
+{
+    use OpenAPIResponseHandlerTrait;
+    use OpenAPIRequestPreparationTrait;
+    use ParametersMakerTrait;
+
+    private const SIGN_FIELD = 'sign';
+
+    private const CONTENT_FIELD = 'alipay_trade_cancel_response';
+
+    private $config;
+
+    private $params = [
+        'app_auth_token' => null,
+    ];
+
+    private $bizContent = [
+        'trade_no' => null,
+        'out_trade_no' => null,
+    ];
+
+    public function __construct(AlipayConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function makeParameters(): array
+    {
+        ParameterHelper::checkRequired($this->bizContent, [], ['trade_no', 'out_trade_no']);
+
+        $bizContent = ParameterHelper::packValidParameters($this->bizContent);
+        $parameters = $this->makeOpenAPISignedParameters('alipay.trade.cancel', $bizContent);
+
+        return $parameters;
+    }
+
+    public function setAppAuthToken(?string $token): self
+    {
+        $this->params['app_auth_token'] = $token;
+
+        return $this;
+    }
+
+    public function setTradeNo(?string $trade_no): self
+    {
+        $this->bizContent['trade_no'] = $trade_no;
+
+        return $this;
+    }
+
+    public function setOutTradeNo(?string $out_trade_no): self
+    {
+        $this->bizContent['out_trade_no'] = $out_trade_no;
+
+        return $this;
+    }
+}
