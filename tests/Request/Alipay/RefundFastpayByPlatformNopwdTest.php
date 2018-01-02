e1334d5e96528b98f2ca02ecdec25486fb65f161
longhaibin <longhaibin@juwang.cn>
2018-01-02 21:00:17 +0800

    支付宝"即时到账批量退款无密接口"单元测试

---
+++ tests/Request/Alipay/RefundFastpayByPlatformNopwdTest.php
@@ -0,0 +1,27 @@
+<?php
+use Archman\PaymentLib\Test\Config;
+use Archman\PaymentLib\Test\Config\AlipayConfig;
+use Archman\PaymentLib\Request\Alipay\RefundFastpayByPlatformNopwd;
+
+class RefundFastpayByPlatformNopwdTest extends \PHPUnit\Framework\TestCase
+{
+    public function testMakingParameters()
+    {
+        $cases = Config::get('alipay', 'testCases', 'request', 'RefundFastpayByPlatformNopwd');
+        foreach ($cases as $each) {
+            $configData = Config::get('alipay', 'config', $each['appID']);
+            $config = new AlipayConfig($configData);
+
+            $request = (new RefundFastpayByPlatformNopwd($config))
+                ->setNotifyURL($each['fields']['notify_url'])
+                ->setUseFreezeAmount($each['fields']['use_freeze_amount'])
+                ->setSerialNumber($each['fields']['serial_number'])
+                ->setRefundDate(new DateTime($each['fields']['refund_date']));
+            foreach ($each['fields']['detail_data'] as $d) {
+                $request->addDetailData(...$d);
+            }
+
+            $this->assertEquals($each['parameters'], $request->makeParameters());
+        }
+    }
+}
