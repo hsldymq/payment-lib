ac24d064fd88ba905dd1484a4421f7c77f6fa607
hsldymq <hsldymq@qq.com>
2018-01-03 17:42:01 +0800

    新增google签名验证

---
+++ src/SignatureHelper/Google/Validator.php
@@ -0,0 +1,27 @@
+<?php
+namespace Archman\PaymentLib\SignatureHelper\Google;
+
+use Archman\PaymentLib\ConfigManager\GoogleConfigInterface;
+
+class Validator
+{
+    private $config;
+
+    public function __construct(GoogleConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function validate(string $signature, string $signatureData): bool
+    {
+        $resource = openssl_get_publickey($this->config->getLicenseKey());
+        if (!$resource) {
+            throw new \Exception("Unable To Get Public Key");
+        }
+
+        $isCorrect = openssl_verify($signatureData, base64_decode($signature), $resource) === 1;
+        openssl_free_key($resource);
+
+        return $isCorrect;
+    }
+}
