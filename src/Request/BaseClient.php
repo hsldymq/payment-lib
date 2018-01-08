7941cc6f80541957f432f8648b299c65bbf453fc
hsldymq <hsldymq@qq.com>
2018-01-08 20:54:13 +0800

    去掉Client注册机制

--- src/Request/BaseClient.php
+++ src/Request/BaseClient.php
@@ -7,8 +7,6 @@ use Psr\Http\Message\ResponseInterface;
 
 abstract class BaseClient
 {
-    private static $implement = Client::class;
-
     final public static function sendRequest(RequestableInterface $interface): BaseResponse
     {
         $request = $interface->prepareRequest();
@@ -16,23 +14,6 @@ abstract class BaseClient
         $response = static::doSend($request, $option);
 
         return $interface->handleResponse($response);
-    }
-
-    /**
-     * @param string $class
-     * @usage BaseClient::registerImplement(SomeClientImpl::class);
-     */
-    final public static function registerImplement(string $class)
-    {
-        self::$implement = $class;
-    }
-
-    /**
-     * @return BaseClient
-     */
-    final public static function getImplement()
-    {
-        return self::$implement;
     }
 
     abstract protected static function doSend(RequestInterface $request, RequestOptionInterface $option): ResponseInterface;
