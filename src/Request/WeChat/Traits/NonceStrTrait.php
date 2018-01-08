01d651acdb04bf673e09d3cc779be934d8ba6edc
hsldymq <hsldymq@qq.com>
2018-01-08 20:37:08 +0800

    微信noncestr trait增加setter

--- src/Request/WeChat/Traits/NonceStrTrait.php
+++ src/Request/WeChat/Traits/NonceStrTrait.php
@@ -3,8 +3,17 @@ namespace Archman\PaymentLib\Request\WeChat\Traits;
 
 trait NonceStrTrait
 {
+    private $nonceStr = null;
+
+    public function setNonceStr(?string $str): self
+    {
+        $this->nonceStr = $str;
+
+        return $this;
+    }
+
     public function getNonceStr(): string
     {
-        return md5(microtime(true));
+        return $this->nonceStr ?? md5(microtime(true));
     }
 }
