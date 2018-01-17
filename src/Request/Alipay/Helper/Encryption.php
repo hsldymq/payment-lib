20a4d72670bb2eadb917a53fb1d4382f31c45ac7
hsldymq <hsldymq@qq.com>
2018-01-17 21:32:04 +0800

    新增加解密helper类,用于支付包open api的AES加解密

---
+++ src/Request/Alipay/Helper/Encryption.php
@@ -0,0 +1,31 @@
+<?php
+namespace Archman\PaymentLib\Request\Alipay\Helper;
+
+class Encryption
+{
+    /**
+     * 当前仅支持AES/CBC/PKCS5Padding.
+     * @param string $data
+     * @param string $encryptedKey
+     * @return string
+     */
+    public static function encrypt(string $data, string $encryptedKey): string
+    {
+        $iv = implode('', array_fill(0, 16, chr(0)));
+
+        return openssl_encrypt($data, 'AES-128-CBC', base64_decode($encryptedKey), 0, $iv);
+    }
+
+    /**
+     * 当前仅支持AES/CBC/PKCS5Padding.
+     * @param string $encryptedData
+     * @param string $decryptedKey
+     * @return string
+     */
+    public static function decrypt(string $encryptedData, string $decryptedKey): string
+    {
+        $iv = implode('', array_fill(0, 16, chr(0)));
+
+        return openssl_decrypt($encryptedData, 'AES-128-CBC', base64_decode($decryptedKey), 0, $iv);
+    }
+}
