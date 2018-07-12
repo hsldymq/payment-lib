198d04a124714a15bf85952410d8d4992504ca52
hsldymq <hsldymq@qq.com>
2018-07-12 10:54:25 +0800

    数据解析器完善XML解析规则

--- src/Request/DataParser.php
+++ src/Request/DataParser.php
@@ -3,15 +3,51 @@ namespace Archman\PaymentLib\Request;
 
 class DataParser
 {
-    public static function arrayToXML(array $data): string
+    public static function arrayToXML(array $data, string $root = 'xml'): string
     {
-        // TODO
+        $xml     = $root ? "<{$root}>%s</{$root}>" : '%s';
+        $content = '';
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                // 允许嵌套
+                $content .= "<{$key}>".self::arrayToXML($value, '')."</{$key}>";
+            } else {
+                try {
+                    $content .= "<{$key}><![CDATA[".strval($value)."]]></{$key}>";
+                } catch (\Throwable $e) {
+                    // TODO throw correct exception
+                    throw $e;
+                }
+            }
+        }
+
+        return sprintf($xml, $content);
     }
 
-    public static function xmlToArray(string $data): array
+    public static function xmlToArray(string $xml): array
     {
-        // TODO
-        // TODO GBK编码使用simplexml库会报错
+        $parse_function = function (\SimpleXMLElement $element) use (&$parse_function) {
+            $data = [];
+            /** @var \SimpleXMLElement $value */
+            foreach ($element as $key => $value) {
+                if ($child = $value->children()) {
+                    $data[$key] = $parse_function($child);
+                } else {
+                    $data[$key] = (string) $value;
+                }
+            }
+
+            return $data;
+        };
+        try {
+            $document = simplexml_load_string($xml);
+            $data     = $parse_function($document);
+        } catch (\Throwable $e) {
+            // TODO throw correct exception
+            throw new \Exception("Parse XML Document Failed: {$e->getMessage()}", $e->getCode(), $e);
+        }
+
+        return $data;
     }
 
     public static function formDataToArray(string $data): array
