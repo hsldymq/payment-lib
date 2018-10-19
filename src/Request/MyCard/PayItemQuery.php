f4e5f0780705b58a31f2c564494ca4135e9d9101
hsldymq <hsldymq@qq.com>
2018-10-19 12:10:01 +0800

    新增MyCard品项查询接口类

---
+++ src/Request/MyCard/PayItemQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Archman\PaymentLib\Request\MyCard;
+
+use Archman\PaymentLib\ConfigManager\MyCardConfigInterface;
+use Archman\PaymentLib\Request\MyCard\Traits\RequestPreparationTrait;
+use Archman\PaymentLib\Request\MyCard\Traits\ResponseHandlerTrait;
+use Archman\PaymentLib\Request\ParameterMakerInterface;
+use Archman\PaymentLib\Request\RequestableInterface;
+
+/**
+ * 付费方式和品项代码查询.
+ */
+class PayItemQuery implements RequestableInterface, ParameterMakerInterface
+{
+    use RequestPreparationTrait;
+    use ResponseHandlerTrait;
+
+    private const TEST_URI = 'https://test.b2b.mycard520.com.tw/MyBillingPay/v1.1/PayItemQuery';
+
+    private const PROD_URI = 'https://b2b.mycard520.com.tw/MyBillingPay/v1.1/PayItemQuery';
+
+    private $config;
+
+    public function __construct(MyCardConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function makeParameters(): array
+    {
+        $parameters = [
+            'FacServiceID' => $this->config->getFacServiceID(),
+        ];
+
+        return $parameters;
+    }
+}
