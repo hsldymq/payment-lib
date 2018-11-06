ac8e3f1c2bff4b555412657af401055669932def
hsldymq <hsldymq@qq.com>
2018-11-06 15:28:17 +0800

    调整微信批量拉取订单评论接口,下行特有的Response

--- src/Request/WeChat/BatchQueryBillComment.php
+++ src/Request/WeChat/BatchQueryBillComment.php
@@ -3,7 +3,9 @@
 namespace Archman\PaymentLib\Request\WeChat;
 
 use Archman\PaymentLib\ConfigManager\WeChatConfigInterface;
+use Archman\PaymentLib\Exception\ErrorResponseException;
 use Archman\PaymentLib\Exception\InvalidParameterException;
+use Archman\PaymentLib\Request\DataParser;
 use Archman\PaymentLib\Request\ParameterHelper;
 use Archman\PaymentLib\Request\ParameterMakerInterface;
 use Archman\PaymentLib\Request\RequestableInterface;
@@ -11,8 +13,10 @@ use Archman\PaymentLib\Request\RequestOption;
 use Archman\PaymentLib\Request\WeChat\Traits\EnvironmentTrait;
 use Archman\PaymentLib\Request\WeChat\Traits\NonceStrTrait;
 use Archman\PaymentLib\Request\WeChat\Traits\RequestPreparationTrait;
-use Archman\PaymentLib\Request\WeChat\Traits\ResponseHandlerTrait;
+use Archman\PaymentLib\Response\BaseResponse;
+use Archman\PaymentLib\Response\WeChat\BillCommentResponse;
 use Archman\PaymentLib\SignatureHelper\WeChat\Generator;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * 拉取订单评价数据.
@@ -24,7 +28,6 @@ class BatchQueryBillComment implements RequestableInterface, ParameterMakerInter
     use NonceStrTrait;
     use EnvironmentTrait;
     use RequestPreparationTrait;
-    use ResponseHandlerTrait;
 
     private const FIXED_SIGN_TYPE = 'HMAC-SHA256';
 
@@ -52,7 +55,7 @@ class BatchQueryBillComment implements RequestableInterface, ParameterMakerInter
         $parameters['appid'] = $this->config->getAppID();
         $parameters['mch_id'] = $this->config->getMerchantID();
         $parameters['nonce_str'] = $this->getNonceStr();
-        $parameters['sign_type'] = self::FIXED_SIGN_TYPE;
+        // $parameters['sign_type'] = self::FIXED_SIGN_TYPE; // 不填写签名类型,否则微信会返回签名错误
         $parameters['sign'] = (new Generator($this->config))->makeSign($parameters, self::FIXED_SIGN_TYPE);
 
         return $parameters;
@@ -93,9 +96,38 @@ class BatchQueryBillComment implements RequestableInterface, ParameterMakerInter
     protected function customRequestOption(RequestOption $option): RequestOption
     {
         $option->setRootCAFilePath($this->config->getRootCAPath())
+            ->setSSLKeyFilePath($this->config->getSSLKeyPath())
+            ->setSSLKeyPassword($this->config->getSSLKeyPassword())
             ->setSSLCertFilePath($this->config->getClientCertPath())
             ->setSSLCertPassword($this->config->getClientCertPassword());
 
         return $option;
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return BaseResponse
+     * @throws
+     */
+    public function handleResponse(ResponseInterface $response): BaseResponse
+    {
+        $rawBody = strval($response->getBody());
+
+        $errCode = $errMsg = null;
+        if (strpos($rawBody, '<xml>') === 0) {
+            $data = DataParser::xmlToArray($rawBody);
+            if (strtoupper($data['return_code']) !== 'SUCCESS') {
+                $errCode = $data['return_code'];
+                $errMsg = $data['return_msg'];
+            } elseif (strtoupper($data['result_code']) !== 'SUCCESS') {
+                $errCode = $data['err_code'];
+            }
+        }
+        if ($errCode) {
+            throw new ErrorResponseException($errCode, $errMsg, $data);
+        }
+
+        return new BillCommentResponse($rawBody);
     }
 }
---
+++ src/Response/WeChat/BillCommentResponse.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Archman\PaymentLib\Response\WeChat;
+
+use Archman\PaymentLib\Response\BaseResponse;
+
+class BillCommentResponse extends BaseResponse implements \Iterator
+{
+    /** @var int */
+    private $offset;
+
+    private $position = 0;
+
+    /**
+     * @var array [
+     *      [
+     *          'datetime' => '评论时间',
+     *          'orderID' => '支付订单号',
+     *          'rating' => '评论星级',
+     *          'comment' => '评论内容',
+     *      ],
+     *      ...
+     * ]
+     */
+    private $comments = [];
+
+    public function __construct(string $comments)
+    {
+        parent::__construct([]);
+
+        $rows = explode("\n", $comments);
+        foreach ($rows as $index => $eachRow) {
+            $eachRow = ltrim($eachRow, '`');
+
+            if ($index === 0) {
+                $this->offset = intval($eachRow);
+                continue;
+            }
+            if (!$eachRow) {
+                continue;
+            }
+
+            $fields = explode(',`', $eachRow);
+            $comment['time'] = array_shift($fields);
+            $comment['orderID'] = array_shift($fields);
+            $comment['rating'] = array_shift($fields);
+            if (count($comment) > 1) {
+                $fields[0] = implode(',`', $fields);
+            }
+            $comment['comment'] = array_shift($fields);
+
+            $this->comments[] = $comment;
+        }
+    }
+
+    public function getOffset(): int
+    {
+        return $this->offset;
+    }
+
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    public function valid()
+    {
+        return isset($this->comments[$this->position]);
+    }
+
+    public function current()
+    {
+        return $this->comments[$this->position];
+    }
+
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        ++$this->position;
+    }
+}
