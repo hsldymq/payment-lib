1b1b9022c582b59b4f56cc34f2fff2fc6540431f
hsldymq <hsldymq@qq.com>
2019-09-19 20:02:58 +0800

    strict types

--- src/ConfigManager/MyCardConfigInterface.php
+++ src/ConfigManager/MyCardConfigInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Archman\PaymentLib\ConfigManager;
 
 interface MyCardConfigInterface
--- src/Exception/ErrorResponseException.php
+++ src/Exception/ErrorResponseException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Archman\PaymentLib\Exception;
 
 class ErrorResponseException extends \Exception
