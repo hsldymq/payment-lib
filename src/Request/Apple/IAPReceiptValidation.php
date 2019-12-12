69789cc09fcd721f197ca412b60c80cb2f44cb61
hsldymq <hsldymq@qq.com>
2019-12-12 23:13:39 +0800

    update apple iap validation

--- src/Request/Apple/IAPReceiptValidation.php
+++ src/Request/Apple/IAPReceiptValidation.php
@@ -1,18 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Archman\PaymentLib\Request\Apple;
 
 use Archman\PaymentLib\Exception\ErrorResponseException;
+use Archman\PaymentLib\Request\BaseClient;
+use Archman\PaymentLib\Request\Client;
 use Archman\PaymentLib\Request\RequestOption;
 use Archman\PaymentLib\Request\RequestOptionInterface;
-use Archman\PaymentLib\Response\BaseResponse;
 use Archman\PaymentLib\Response\GeneralResponse;
 use GuzzleHttp\Psr7\Request;
 use function GuzzleHttp\Psr7\stream_for;
 use GuzzleHttp\Psr7\Uri;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Archman\PaymentLib\Request\DataParser;
+use Archman\PaymentLib\Request\DataConverter;
 use Archman\PaymentLib\Request\ParameterHelper;
 use Archman\PaymentLib\Request\RequestableInterface;
 
@@ -25,14 +28,21 @@ class IAPReceiptValidation implements RequestableInterface
 
     private const PRODUCTION_URI = 'https://buy.itunes.apple.com/verifyReceipt';
 
-    private $params = [
+    private array $params = [
         'receipt-data' => null,
         'password' => null,
         'exclude-old-transactions' => null,
     ];
 
-    private $uri = null;
+    private ?string $uri = null;
 
+    /**
+     * 设置请求环境
+     *
+     * @param bool $isProduction 是否向生产环境发起请求
+     *
+     * @return $this
+     */
     public function setEnvironment(bool $isProduction): self
     {
         $this->uri = $isProduction ? self::PRODUCTION_URI : self::SANDBOX_URI;
@@ -61,28 +71,21 @@ class IAPReceiptValidation implements RequestableInterface
         return $this;
     }
 
+    public function send(?BaseClient $client = null): GeneralResponse
+    {
+        $response = $client ? $client->sendRequest($this) : Client::send($this);
+
+        return $this->handleResponse($response);
+    }
+
     public function prepareRequest(): RequestInterface
     {
-        ParameterHelper::checkRequired($this->params, ['receipt-data']);
-
         $uri = $this->getUri();
         $headers = ['content-type' => 'application/x-www-form-urlencoded'];
         $params = ParameterHelper::packValidParameters($this->params);
-        $body = stream_for(DataParser::arrayToJson($params));
+        $body = stream_for(DataConverter::arrayToJson($params));
 
         return new Request('POST', $uri, $headers, $body);
-    }
-
-    public function handleResponse(ResponseInterface $response): BaseResponse
-    {
-        $data = DataParser::jsonToArray(strval($response->getBody()));
-
-        $status = strval($data['status']);
-        if ($status !== '0') {
-            throw new ErrorResponseException($data['status'], null, $data);
-        }
-
-        return new GeneralResponse($data);
     }
 
     public function prepareRequestOption(): RequestOptionInterface
@@ -90,10 +93,21 @@ class IAPReceiptValidation implements RequestableInterface
         return new RequestOption();
     }
 
+    private function handleResponse(ResponseInterface $response): GeneralResponse
+    {
+        $data = DataConverter::jsonToArray($response->getBody()->getContents());
+        $status = strval($data['status']);
+        if ($status !== '0') {
+            throw new ErrorResponseException($status, null, $data);
+        }
+
+        return new GeneralResponse($data);
+    }
+
     private function getUri(): Uri
     {
         if (!$this->uri) {
-            throw new \Exception("Set Environment!!!");
+            throw new \Exception("set environment first");
         }
 
         return new Uri($this->uri);
