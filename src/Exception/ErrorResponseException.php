df4e6bb85c84cb1b295b51f2356befc7685a0fc0
hsldymq <hsldymq@qq.com>
2020-04-09 16:14:02 +0800

    refactor error response exception

--- src/Exception/ErrorResponseException.php
+++ src/Exception/ErrorResponseException.php
@@ -12,21 +12,13 @@ class ErrorResponseException extends \Exception
 
     private ?array $responseData = null;
 
-    /**
-     * @param string|null $errorCode
-     * @param string|null $errorText
-     * @param array|null $responseData
-     * @param string $message
-     * @param int $code
-     * @param \Throwable|null $previous
-     */
     public function __construct(
         ?string $errorCode = null,
         ?string $errorText = null,
         ?array $responseData = null,
-        $message = "",
-        $code = 0,
-        \Throwable $previous = null
+        string $message = "",
+        int $code = 0,
+        ?\Throwable $previous = null
     ) {
         $this->errorCode = $errorCode;
         $this->errorText = $errorText;
@@ -35,25 +27,16 @@ class ErrorResponseException extends \Exception
         parent::__construct($message, $code, $previous);
     }
 
-    /**
-     * @return string|null
-     */
     public function getErrorCode(): ?string
     {
         return $this->errorCode;
     }
 
-    /**
-     * @return string|null
-     */
     public function getErrorText(): ?string
     {
         return $this->errorText;
     }
 
-    /**
-     * @return array|null
-     */
     public function getResponseData(): ?array
     {
         return $this->responseData;
