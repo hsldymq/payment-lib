3974436ad6b4733c26fe0e4aac9d1e862e9ed335
hsldymq <hsldymq@qq.com>
2020-11-18 18:28:03 +0800

    update: comment

--- src/Alipay/Config/OpenAPIConfigInterface.php
+++ src/Alipay/Config/OpenAPIConfigInterface.php
@@ -7,23 +7,23 @@ namespace Archman\PaymentLib\Alipay\Config;
 interface OpenAPIConfigInterface extends ConfigInterface
 {
     /**
-     * 返回签名算法名称.
+     * 返回签名类型.
      *
      * @return string RSA/RSA2
      */
-    public function getSignAlgo(): string;
+    public function getSignType(): string;
 
     /**
      * 返回应用私钥.
      *
-     * @return string 返回私钥文件或者私钥内容, 如果是私钥内容必须为PEM格式.
+     * @return string 返回私钥文件路径或者私钥内容, 如果是私钥内容必须为PEM格式.
      */
     public function getPrivateKey(): string;
 
     /**
      * 返回支付宝公钥.
      *
-     * @return string 返回公钥文件或者公钥内容, 如果是公钥内容必须为PEM格式.
+     * @return string 返回公钥文件路径或者公钥内容, 如果是公钥内容必须为PEM格式.
      */
     public function getAlipayPublicKey(): string;
 
@@ -35,4 +35,11 @@ interface OpenAPIConfigInterface extends ConfigInterface
      * @see https://docs.open.alipay.com/common/104567 查看关于AES加密接口内容的作用
      */
     public function getAESKey(): ?string;
+
+    /**
+     * 是否开启了AES加密.
+     *
+     * @return bool
+     */
+    public function isAESEncryptionEnabled(): bool;
 }
