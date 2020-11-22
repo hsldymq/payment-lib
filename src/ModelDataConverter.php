d8bb66399d89b3bf3cc046110ed4470a07057664
hsldymq <hsldymq@qq.com>
2020-11-22 15:19:02 +0800

    add: converter

---
+++ src/ModelDataConverter.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archman\PaymentLib;
+
+class ModelDataConverter
+{
+    public static function defaultConvert($data, string $targetType)
+    {
+        // TODO
+        return '';
+    }
+}
