b13c0483718f6ee4f49d52ec087eede660ee4a68
hsldymq <hsldymq@qq.com>
2020-11-30 15:06:32 +0800

    update comments

--- src/Exception/SignGenerationException.php
+++ src/Exception/SignGenerationException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Archman\PaymentLib\Exception;
 
+/**
+ * 生成签名时产生的异常.
+ */
 class SignGenerationException extends ContextualException
 {
 }
--- src/Exception/SignValidationException.php
+++ src/Exception/SignValidationException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Archman\PaymentLib\Exception;
 
+/**
+ * 验证签名时产生的异常.
+ */
 class SignValidationException extends ContextualException
 {
 }
