1bac1966f16ae8c4dbe3d89518a4850949ea407b
hsldymq <hsldymq@qq.com>
2020-11-30 16:03:21 +0800

    refactor: trade refund test

--- tests/Alipay/TradeRefundTest.php
+++ tests/Alipay/TradeRefundTest.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Archman\PaymentLib\Test\Alipay;
 
 use Archman\PaymentLib\Alipay\TradeRefund;
 use Archman\PaymentLib\Test\Alipay\Config\OpenAPIConfig;
 use PHPUnit\Framework\TestCase;
 use Archman\PaymentLib\Test\Config;
-use Archman\PaymentLib\Test\Config\AlipayConfig;
 
 class TradeRefundTest extends TestCase
 {
@@ -16,7 +17,7 @@ class TradeRefundTest extends TestCase
         foreach ($cases as $each) {
             $configData = Config::get('alipay', 'config', $each['appID']);
             $config = new OpenAPIConfig($configData, $each['signType']);
-            $config->enableAESEncrypt($each['encrypted']);
+            $config->enableAESEncrypt($each['encrypted'] ?? false);
 
             $request = (new TradeRefund($config))
                 ->setOutTradeNo($each['fields']['out_trade_no'])
