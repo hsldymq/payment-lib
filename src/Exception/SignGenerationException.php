f90cb6eebc585e8e4760485a5f81250ee38ac52c
hsldymq <hsldymq@qq.com>
2020-11-30 15:08:22 +0800

    update: comments

--- src/Exception/SignGenerationException.php
+++ src/Exception/SignGenerationException.php
@@ -6,6 +6,8 @@ namespace Archman\PaymentLib\Exception;
 
 /**
  * 生成签名时产生的异常.
+ *
+ * 通过 getContext 方法获取上下文信息.
  */
 class SignGenerationException extends ContextualException
 {
--- src/Exception/SignValidationException.php
+++ src/Exception/SignValidationException.php
@@ -6,6 +6,8 @@ namespace Archman\PaymentLib\Exception;
 
 /**
  * 验证签名时产生的异常.
+ *
+ * 通过 getContext 方法获取上下文信息.
  */
 class SignValidationException extends ContextualException
 {
