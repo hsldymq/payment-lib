42605218912a8fb3d8e6a3f02ba75491493df9b1
hsldymq <hsldymq@qq.com>
2020-12-01 11:19:17 +0800

    update: test for Alipay TradeCancel

--- tests/Alipay/TradeCancelTest.php
+++ tests/Alipay/TradeCancelTest.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace Archman\PaymentLib\Test\Request\Alipay;
+declare(strict_types=1);
 
-use Archman\PaymentLib\Request\Alipay\TradeCancel;
+namespace Archman\PaymentLib\Test\Alipay;
+
+use Archman\PaymentLib\Alipay\TradeCancel;
+use Archman\PaymentLib\Test\Alipay\Config\OpenAPIConfig;
 use PHPUnit\Framework\TestCase;
 use Archman\PaymentLib\Test\Config;
-use Archman\PaymentLib\Test\Config\AlipayConfig;
 
 class TradeCancelTest extends TestCase
 {
@@ -14,13 +16,13 @@ class TradeCancelTest extends TestCase
         $cases = Config::get('alipay', 'testCases', 'request', 'TradeCancel');
         foreach ($cases as $each) {
             $configData = Config::get('alipay', 'config', $each['appID']);
-            $config = new AlipayConfig($configData);
-            $config->setOpenAPIDefaultSignType($each['signType']);
+            $config = new OpenAPIConfig($configData, $each['signType']);
+            $config->enableAESEncrypt($each['encrypted'] ?? false);
+
 
             $request = (new TradeCancel($config))
                 ->setOutTradeNo($each['fields']['out_trade_no'])
-                ->setTimestamp(new \DateTime($each['fields']['timestamp']))
-                ->encrypt($each['encrypted']);
+                ->setTimestamp(new \DateTime($each['fields']['timestamp']));
 
             $this->assertEquals($each['parameters'], $request->makeParameters());
         }
