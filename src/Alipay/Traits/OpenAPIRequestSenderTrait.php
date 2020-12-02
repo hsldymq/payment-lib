23258d69dd038e9e9159644d9136cc4586063350
hsldymq <hsldymq@qq.com>
2020-12-02 12:59:06 +0800

    fix: OpenAPIRequestSenderTrait在启动加密时,获取响应没有解密数据

--- src/Alipay/Traits/OpenAPIRequestSenderTrait.php
+++ src/Alipay/Traits/OpenAPIRequestSenderTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Archman\PaymentLib\Alipay\Traits;
 
 use Archman\PaymentLib\Alipay\Config\OpenAPIConfigInterface;
+use Archman\PaymentLib\Alipay\Helper\AESEncryption;
 use Archman\PaymentLib\Alipay\Helper\OpenAPIHelper;
 use Archman\PaymentLib\ClientFactoryInterface;
 use Archman\PaymentLib\ClientOption;
@@ -22,6 +23,8 @@ use Psr\Http\Message\ResponseInterface;
  */
 trait OpenAPIRequestSenderTrait
 {
+    public static string $baseURI = 'https://openapi.alipay.com/gateway.do';
+
     private array $latestRequestInfo = [
         'request' => null,
         'response' => null,
@@ -50,6 +53,15 @@ trait OpenAPIRequestSenderTrait
         (new Validator($this->config))->validateSign($signature, $this->config->getSignType(), $contentStr);
 
         $content = $data[self::RESPONSE_CONTENT_FIELD];
+        if ($this->config->isAESEncryptionEnabled()) {
+             $d = AESEncryption::decrypt($content, $this->config->getAESKey());
+             try {
+                 $content = json_decode($d, true, 512, JSON_THROW_ON_ERROR);
+             } catch (\Throwable $e) {
+                 throw new InvalidDataStructureException($d, [], $e->getMessage(), $e->getCode(), $e);
+             }
+        }
+
         $code = strval($content['code'] ?? '');
         if ($code !== '10000') {
             throw new AlipayOpenAPIResponseException($content, $content['msg'] ?? '', $code);
@@ -66,7 +78,7 @@ trait OpenAPIRequestSenderTrait
     protected function doSend(ClientInterface $client): ResponseInterface
     {
         $parameters = $this->makeParameters();
-        $request = new Request('POST','https://openapi.alipay.com/gateway.do?'.Query::build($parameters));
+        $request = new Request('POST', self::$baseURI.'?'.Query::build($parameters));
 
         $this->latestRequestInfo = ['request' => $request, 'response' => null];
 
