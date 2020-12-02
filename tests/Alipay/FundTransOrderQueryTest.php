38e79eded650a9d93707432d1796799dfce100ff
hsldymq <hsldymq@qq.com>
2020-12-02 13:00:17 +0800

    update: FundTransOrderQueryTest

--- tests/Alipay/FundTransOrderQueryTest.php
+++ tests/Alipay/FundTransOrderQueryTest.php
@@ -1,11 +1,13 @@
 <?php
-namespace Archman\PaymentLib\Test\Request\Alipay;
 
+declare(strict_types=1);
+
+namespace Archman\PaymentLib\Test\Alipay;
+
+use Archman\PaymentLib\Test\Alipay\Config\OpenAPIConfig;
 use PHPUnit\Framework\TestCase;
 use Archman\PaymentLib\Test\Config;
-use Archman\PaymentLib\Test\Config\AlipayConfig;
-use Archman\PaymentLib\Request\Alipay\FundTransToAccountTransfer;
-use Archman\PaymentLib\Request\Alipay\FundTransOrderQuery;
+use Archman\PaymentLib\Alipay\FundTransOrderQuery;
 
 class FundTransOrderQueryTest extends TestCase
 {
@@ -14,11 +16,10 @@ class FundTransOrderQueryTest extends TestCase
         $cases = Config::get('alipay', 'testCases', 'request', 'FundTransOrderQuery');
         foreach ($cases as $each) {
             $configData = Config::get('alipay', 'config', $each['appID']);
-            $config = new AlipayConfig($configData);
-            $config->setOpenAPIDefaultSignType($each['signType']);
+            $config = new OpenAPIConfig($configData, $each['signType']);
+            $config->enableAESEncrypt($each['encrypted'] ?? false);
 
             $request = (new FundTransOrderQuery($config))
-                ->encrypt($each['encrypted'])
                 ->setTimestamp(new \DateTime($each['fields']['timestamp']))
                 ->setOrderID($each['fields']['order_id'] ?? null)
                 ->setOutBizNo($each['fields']['out_biz_no'] ?? null);
