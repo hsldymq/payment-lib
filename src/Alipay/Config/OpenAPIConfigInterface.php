b7937c5e8e0c701a1ced51f406b734f961f8ec1d
hsldymq <hsldymq@qq.com>
2020-12-02 17:05:27 +0800

    update: OpenAPIConfigInterface. 增加获取证书接口

--- src/Alipay/Config/OpenAPIConfigInterface.php
+++ src/Alipay/Config/OpenAPIConfigInterface.php
@@ -28,6 +28,34 @@ interface OpenAPIConfigInterface extends ConfigInterface
     public function getAlipayPublicKey(): string;
 
     /**
+     * 获得应用公钥证书.
+     *
+     * @return string 返回证书文件路径或者证书内容, 如果是公钥内容必须为PEM格式.
+     */
+    public function getCert(): string;
+
+    /**
+     * 获得支付宝公钥根证书.
+     *
+     * @return string 返回证书文件路径或者证书内容, 如果是公钥内容必须为PEM格式.
+     */
+    public function getAlipayRootCert(): string;
+
+    /**
+     * 获得支付宝公钥证书.
+     *
+     * @return string 返回证书文件路径或者证书内容, 如果是公钥内容必须为PEM格式.
+     */
+    public function getAlipayCert(): string;
+
+    /**
+     * 是否开启公钥证书, 返回true则会在请求中增加证书序列号, 验证响应时也会使用证书公钥进行验签名.
+     *
+     * @return bool
+     */
+    public function isCertEnabled(): bool;
+
+    /**
      * 返回AES密钥.
      *
      * @return string
