34ee095b0d7419314c8c86e02a91b2f07fe3c420
hsldymq <hsldymq@qq.com>
2020-12-02 13:00:00 +0800

    add: FunTransOrderQuery.php

---
+++ src/Alipay/FundTransOrderQuery.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archman\PaymentLib\Alipay;
+
+use Archman\PaymentLib\Alipay\Config\OpenAPIConfigInterface;
+use Archman\PaymentLib\Alipay\Traits\OpenAPIExtendableTrait;
+use Archman\PaymentLib\Alipay\Traits\OpenAPIParameterTrait;
+use Archman\PaymentLib\Alipay\Traits\OpenAPIRequestSenderTrait;
+use Archman\PaymentLib\Request\ParameterMakerInterface;
+
+/**
+ * 查询转账订单接口.
+ *
+ * @see https://docs.open.alipay.com/api_28/alipay.fund.trans.order.query 接口文档
+ */
+class FundTransOrderQuery implements ParameterMakerInterface
+{
+    use OpenAPIExtendableTrait;
+    use OpenAPIParameterTrait;
+    use OpenAPIRequestSenderTrait;
+
+    private const METHOD = 'alipay.fund.trans.order.query';
+    private const VERSION = '1.0';
+    private const CHARSET = 'utf-8';
+    private const WITH_CERT = false;
+    private const RESPONSE_CONTENT_FIELD = 'alipay_fund_trans_order_query_response';
+
+    private OpenAPIConfigInterface $config;
+
+    private array $params = [
+        'timestamp' => null,
+    ];
+
+    private array $bizContent = [
+        'out_biz_no' => null,
+        'order_id' => null,
+    ];
+
+    public function __construct(OpenAPIConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function setOutBizNo(?string $outBizNo): self
+    {
+        $this->bizContent['out_biz_no'] = $outBizNo;
+
+        return $this;
+    }
+
+    public function setOrderID(?string $id): self
+    {
+        $this->bizContent['order_id'] = $id;
+
+        return $this;
+    }
+}
