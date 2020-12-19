5953d91ae8fd5772e4badbd34eb03a7513d9abb2
hsldymq <hsldymq@qq.com>
2020-12-19 23:31:19 +0800

    refactor: 调整测试配置文件结构

--- tests/Alipay/Config/OpenAPIConfig.php
+++ tests/Alipay/Config/OpenAPIConfig.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace Archman\PaymentLib\Test\Alipay\Config;
 
 use Archman\PaymentLib\Alipay\Config\OpenAPIConfigInterface;
+use Archman\PaymentLib\Alipay\Helper\CertHelper;
 
 class OpenAPIConfig implements OpenAPIConfigInterface
 {
-    private array $config;
-
-    private string $signType;
-
     private bool $aesEncEnabled = false;
 
-    private bool $certEnabled = false;
-
-    public function __construct(array $config, string $signType)
-    {
-        $this->config = $config;
-        $this->signType = $signType;
+    public function __construct(
+        private array $config,
+        private string $signType,
+        private bool $certEnabled = false
+    ) {
     }
 
     public function getAppID(): string
@@ -39,22 +35,31 @@ class OpenAPIConfig implements OpenAPIConfigInterface
 
     public function getPrivateKey(): string
     {
-        return $this->config['openAPIPrivateKeys'][$this->signType];
+        return $this->config['openAPI'][$this->signType]['appPrivateKey'] ?? '';
     }
 
     public function getAlipayPublicKey(): string
     {
-        return $this->config['openAPIAlipayPublicKeys'][$this->signType];
+        if ($this->certEnabled) {
+            CertHelper::extractPublicKey($this->config['openAPI'][$this->signType]['alipayCert'] ?? '');
+        }
+
+        return $this->config['openAPI'][$this->signType]['alipayPublicKey'] ?? '';
     }
 
     public function getCert(): string
     {
-        return $this->config['openAPIAppCert'] ?? '';
+        return $this->config['openAPI'][$this->signType]['appCert'] ?? '';
     }
 
     public function getAlipayCert(): string
     {
-        return $this->config['openAPIAlipayCert'] ?? '';
+        return $this->config['openAPI'][$this->signType]['alipayCert'] ?? '';
+    }
+
+    public function getAlipayRootCert(): string
+    {
+        return $this->config['openAPI'][$this->signType]['alipayRootCert'] ?? '';
     }
 
     public function isCertEnabled(): bool
@@ -62,19 +67,9 @@ class OpenAPIConfig implements OpenAPIConfigInterface
         return $this->certEnabled;
     }
 
-    public function enableCert(bool $enabled)
-    {
-        $this->certEnabled = $enabled;
-    }
-
-    public function getAlipayRootCert(): string
-    {
-        return $this->config['openAPIAlipayRootCert'] ?? '';
-    }
-
     public function getAESKey(): string
     {
-        return $this->config['openAPIEncryptionKey'];
+        return $this->config['openAPI']['aesKey'];
     }
 
     public function isAESEncryptionEnabled(): bool
