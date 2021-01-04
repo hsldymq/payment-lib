f743df4f6181241da74813f25f9a606524eabd89
hsldymq <hsldymq@qq.com>
2021-01-04 17:40:53 +0800

    update: 晚上DataModel

--- src/DataModel.php
+++ src/DataModel.php
@@ -5,28 +5,63 @@ declare(strict_types=1);
 namespace Archman\PaymentLib;
 
 use Archman\PaymentLib\Exception\ContextualException;
-use JetBrains\PhpStorm\Immutable;
+use JetBrains\PhpStorm\Pure;
 
-#[Immutable]
-class DataModel implements \ArrayAccess
+class DataModel implements \ArrayAccess, \Iterator
 {
-    private static array $converters = [
-        'default' => [ModelDataConverter::class, 'defaultConvert'],
-    ];
-
     private array $data = [];
+    private array $keys = [];
+    private int $numKeys = 0;
+    private int $position = 0;
 
     final public function __construct(array $data)
     {
         $this->data = $data;
+        $this->numKeys = count($data);
+        $this->keys = array_keys($data);
         $this->assignProps();
     }
 
+    #[Pure]
+    public function current(): mixed
+    {
+        if ($this->position >= $this->numKeys) {
+            return null;
+        }
+
+        $key = $this->keys[$this->position];
+        return $this->data[$key];
+    }
+
+    public function next(): void
+    {
+        $this->position += ($this->position < $this->numKeys ? 1 : 0);
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    #[Pure]
+    public function key(): string|float|int|bool|null
+    {
+        return $this->position < $this->numKeys ? $this->keys[$this->position] : null;
+    }
+
+    #[Pure]
+    public function valid(): bool
+    {
+        return $this->position >= $this->numKeys;
+    }
+
+    #[Pure]
     public function offsetExists($offset): bool
     {
         return isset($this->data[$offset]);
     }
 
+    #[Pure]
     public function offsetGet($offset): mixed
     {
         return $this->data[$offset] ?? null;
@@ -55,18 +90,34 @@ class DataModel implements \ArrayAccess
                 continue;
             }
 
-//            $converterAttr = $prop->getAttributes('DataConverter')[0] ?? null;
-//            if ($converterAttr) {
-//                if ($typeStr) {
-//                    $converterName = 'default';
-//
-//                    if ($attr && ($name = $attr->getArguments()[0] ?? null)) {
-//                        $converterName = $name;
-//                    }
-//                }
-//            }
+            $value = $this->data[$fieldName];
 
-            $prop->setValue($this, $this->data[$fieldName]);
+            $converterAttr = $prop->getAttributes('DataConverter')[0] ?? null;
+            if ($converterAttr) {
+                $converterName = strval($converterAttr->getArguments()[0] ?? '');
+                $converter = DataConverterFactory::getConverter($converterName);
+                $types = $this->getPropTypes($prop);
+                if ($types) {
+                    $value = $converter($value, $types);
+                }
+            }
+
+            $prop->setValue($this, $value);
         }
+    }
+
+    private function getPropTypes(\ReflectionProperty $prop): array
+    {
+        $types = [];
+        $type = $prop->getType();
+        if ($type instanceof \ReflectionNamedType) {
+            $types[] = $type->getName();
+        } else if ($type instanceof \ReflectionUnionType) {
+            foreach ($type->getTypes() as $eachType) {
+                $types[] = $eachType->getName();
+            }
+        }
+
+        return $types;
     }
 }
